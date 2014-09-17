23065c73379687f2f6042f53bcdb629ebe43d242
Terry Sun <terry.github@gmail.com>
2014-09-17 23:10:03 +0800

    submit project to git

---
+++ MainWindow.cs
@@ -0,0 +1,373 @@
+/****************************************************************************
+*                                                                           *
+*  OpenNI 1.x Alpha                                                         *
+*  Copyright (C) 2011 PrimeSense Ltd.                                       *
+*                                                                           *
+*  This file is part of OpenNI.                                             *
+*                                                                           *
+*  OpenNI is free software: you can redistribute it and/or modify           *
+*  it under the terms of the GNU Lesser General Public License as published *
+*  by the Free Software Foundation, either version 3 of the License, or     *
+*  (at your option) any later version.                                      *
+*                                                                           *
+*  OpenNI is distributed in the hope that it will be useful,                *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+*  GNU Lesser General Public License for more details.                      *
+*                                                                           *
+*  You should have received a copy of the GNU Lesser General Public License *
+*  along with OpenNI. If not, see <http://www.gnu.org/licenses/>.           *
+*                                                                           *
+****************************************************************************/
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenNI;
+using System.Threading;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Collections;
+using System.Diagnostics;
+using GdipEffect;
+using System.Runtime.InteropServices;
+using UserTracker.net.Utilities;
+using ShadowEffect;
+namespace ShadowEffect
+{
+	public partial class MainWindow : Form
+	{
+        FormState formState = new FormState();
+		public MainWindow()
+		{
+			InitializeComponent();
+            //full screen
+            this.Left = this.Top = 0;
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width;
+            this.Height = Screen.PrimaryScreen.WorkingArea.Height;
+            formState.Maximize(this);
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            Taskbar.Hide();
+            
+			this.context = Context.CreateFromXmlFile(SAMPLE_XML_FILE, out scriptNode);
+			this.depth = context.FindExistingNode(NodeType.Depth) as DepthGenerator;
+			if (this.depth == null)
+			{
+				throw new Exception("Viewer must have a depth node!");
+			}
+
+            this.userGenerator = new UserGenerator(this.context);
+			this.skeletonCapbility = this.userGenerator.SkeletonCapability;
+			this.poseDetectionCapability = this.userGenerator.PoseDetectionCapability;
+            this.calibPose = this.skeletonCapbility.CalibrationPose;
+
+            this.userGenerator.NewUser += userGenerator_NewUser;
+            this.userGenerator.LostUser += userGenerator_LostUser;
+            this.poseDetectionCapability.PoseDetected += poseDetectionCapability_PoseDetected;
+            this.skeletonCapbility.CalibrationComplete += skeletonCapbility_CalibrationComplete;
+
+            this.skeletonCapbility.SetSkeletonProfile(SkeletonProfile.All);
+            this.joints = new Dictionary<int,Dictionary<SkeletonJoint,SkeletonJointPosition>>();
+            this.userGenerator.StartGenerating();
+
+
+			this.histogram = new int[this.depth.DeviceMaxDepth];
+
+			MapOutputMode mapMode = this.depth.MapOutputMode;
+
+			this.bitmap = new Bitmap((int)mapMode.XRes, (int)mapMode.YRes/*, System.Drawing.Imaging.PixelFormat.Format24bppRgb*/);
+			this.shouldRun = true;
+			this.readerThread = new Thread(ReaderThread);
+			this.readerThread.Start();
+		}
+
+        void skeletonCapbility_CalibrationComplete(object sender, CalibrationProgressEventArgs e)
+        {
+            if (e.Status == CalibrationStatus.OK)
+            {
+                this.skeletonCapbility.StartTracking(e.ID);
+                this.joints.Add(e.ID, new Dictionary<SkeletonJoint, SkeletonJointPosition>());
+            }
+            else if (e.Status != CalibrationStatus.ManualAbort)
+            {
+                if (this.skeletonCapbility.DoesNeedPoseForCalibration)
+                {
+                    this.poseDetectionCapability.StartPoseDetection(calibPose, e.ID);
+                }
+                else
+                {
+                    this.skeletonCapbility.RequestCalibration(e.ID, true);
+                }
+            }
+        }
+
+        void poseDetectionCapability_PoseDetected(object sender, PoseDetectedEventArgs e)
+        {
+            this.poseDetectionCapability.StopPoseDetection(e.ID);
+            this.skeletonCapbility.RequestCalibration(e.ID, true);
+        }
+
+        void userGenerator_NewUser(object sender, NewUserEventArgs e)
+        {
+            if (this.skeletonCapbility.DoesNeedPoseForCalibration)
+            {
+                this.poseDetectionCapability.StartPoseDetection(this.calibPose, e.ID);
+            }
+            else
+            {
+                this.skeletonCapbility.RequestCalibration(e.ID, true);
+            }
+        }
+
+		void userGenerator_LostUser(object sender, UserLostEventArgs e)
+        {
+            this.joints.Remove(e.ID);
+        }
+        Queue ImgQueue = new Queue();
+        int ImgQueueLitmitation = App.Default.ShadowQuantity;//殘影的數量
+        int ImgCounter = 0;
+        int saveLimit = App.Default.ShadowSpeed;//每幾個影格存一個 //被lock住，還沒有處理完之前，不會有新影像進來
+        
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+
+			lock (this)
+			{
+                //建一個queue，只畫前幾個畫面
+                ImgCounter++;
+                if (App.Default.IsBlackShadow)
+                {
+                    this.bitmap.Invert();
+                }
+                
+                
+
+                if (ImgCounter > saveLimit)
+                {
+                    ImgCounter = 0;
+                    //去背後存入Queue
+                    if (App.Default.IsReverse)
+                    {
+                        this.bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    }
+                    
+                    ImgQueue.Enqueue(new Bitmap(this.bitmap));
+                }
+                //LastImg=合成所有Queue的照片，
+                //把所有Queue的圖合成一張，去背、
+                e.Graphics.DrawImage(MergingAllImage(),
+                this.panelView.Location.X,
+                this.panelView.Location.Y,
+                this.panelView.Size.Width,
+                this.panelView.Size.Height);
+                
+            }
+		}
+        
+        private Bitmap MergingAllImage()
+        {
+            Bitmap LastBitmap = new Bitmap(this.bitmap.Width, this.bitmap.Height);
+            using (Graphics grp = Graphics.FromImage(LastBitmap))
+             {
+                 grp.FillRectangle(
+                     Brushes.White, 0, 0, this.bitmap.Width, this.bitmap.Height);
+             }
+            Rectangle _Rectangle = new Rectangle(0, 0, LastBitmap.Width, LastBitmap.Height);
+            Queue TempQueue = new Queue();
+            
+            LastBitmap.GaussianBlur(ref _Rectangle, App.Default.ShadowBlurRadio);
+            if(ImgQueue.Count > 0)
+            {
+                var canvas = Graphics.FromImage(LastBitmap);
+                while(ImgQueue.Count > 0)
+                {
+                    
+                    Bitmap tmpImg=(Bitmap)ImgQueue.Dequeue();
+                    tmpImg.MakeTransparent(Color.White);
+                    tmpImg.GaussianBlur(ref _Rectangle,App.Default.ShadowBlurRadio);
+                    
+                    if (ImgQueue.Count < ImgQueueLitmitation)
+                    {
+                        TempQueue.Enqueue(tmpImg);
+                    }
+
+                    ColorMatrix cm = new ColorMatrix();
+                    cm.Matrix33 = App.Default.ShadowAlpha;
+                    ImageAttributes _ImageAttributes = new ImageAttributes();
+                    _ImageAttributes.SetColorMatrix(cm);
+
+
+                    canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    canvas.DrawImage(tmpImg, new Rectangle(0, 0, this.panelView.Size.Width, this.panelView.Size.Height),
+                        0, 0, this.panelView.Size.Width, this.panelView.Size.Height,GraphicsUnit.Pixel,_ImageAttributes);
+                    canvas.Save();
+                       
+                }
+                ImgQueue = TempQueue;
+                ImgQueue.TrimToSize();
+            }
+            return LastBitmap;
+        }
+		protected override void OnPaintBackground(PaintEventArgs pevent)
+		{
+			//Don't allow the background to paint
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			this.shouldRun = false;
+			this.readerThread.Join();
+			base.OnClosing(e);
+		}
+		private unsafe void CalcHist(DepthMetaData depthMD)
+		{
+			// reset
+			for (int i = 0; i < this.histogram.Length; ++i)
+				this.histogram[i] = 0;
+
+			ushort* pDepth = (ushort*)depthMD.DepthMapPtr.ToPointer();
+
+			int points = 0;
+			for (int y = 0; y < depthMD.YRes; ++y)
+			{
+				for (int x = 0; x < depthMD.XRes; ++x, ++pDepth)
+				{
+					ushort depthVal = *pDepth;
+					if (depthVal != 0)
+					{
+						this.histogram[depthVal]++;
+						points++;
+					}
+				}
+			}
+
+			for (int i = 1; i < this.histogram.Length; i++)
+			{
+				this.histogram[i] += this.histogram[i-1];
+			}
+
+			if (points > 0)
+			{
+				for (int i = 1; i < this.histogram.Length; i++)
+				{
+					this.histogram[i] = (int)(256 * (1.0f - (this.histogram[i] / (float)points)));
+				}
+			}
+		}
+
+        private Color[] colors = { Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black };
+        private Color[] anticolors = { Color.Green, Color.Orange, Color.Red, Color.Purple, Color.Blue, Color.Yellow, Color.White};
+        private int ncolors = 6;
+
+        private void GetJoint(int user, SkeletonJoint joint)
+        {
+            SkeletonJointPosition pos = this.skeletonCapbility.GetSkeletonJointPosition(user, joint);
+			if (pos.Position.Z == 0)
+			{
+				pos.Confidence = 0;
+			}
+			else
+			{
+				pos.Position = this.depth.ConvertRealWorldToProjective(pos.Position);
+			}
+			this.joints[user][joint] = pos;
+        }
+
+		private unsafe void ReaderThread()
+		{
+			DepthMetaData depthMD = new DepthMetaData();
+
+			while (this.shouldRun)
+			{
+				try
+				{
+					this.context.WaitOneUpdateAll(this.depth);
+				}
+				catch (Exception)
+				{
+				}
+
+				this.depth.GetMetaData(depthMD);
+
+				CalcHist(depthMD);
+
+				lock (this)
+				{
+					Rectangle rect = new Rectangle(0, 0, this.bitmap.Width, this.bitmap.Height);
+					BitmapData data = this.bitmap.LockBits(rect, ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+
+                    if (this.shouldDrawPixels)
+                    {
+                        ushort* pDepth = (ushort*)this.depth.DepthMapPtr.ToPointer();
+                        ushort* pLabels = (ushort*)this.userGenerator.GetUserPixels(0).LabelMapPtr.ToPointer();
+
+                        // set pixels
+                        for (int y = 0; y < depthMD.YRes; ++y)
+                        {
+                            byte* pDest = (byte*)data.Scan0.ToPointer() + y * data.Stride;
+                            for (int x = 0; x < depthMD.XRes; ++x, ++pDepth, ++pLabels, pDest += 3)
+                            {
+                                pDest[0] = pDest[1] = pDest[2] = 0;
+
+                                ushort label = *pLabels;
+                                if (this.shouldDrawBackground || *pLabels != 0)
+                                {
+                                    pDest[0] = (byte)(Color.White.B);
+                                    pDest[1] = (byte)(Color.White.G);
+                                    pDest[2] = (byte)(Color.White.R);
+                                }
+                            }
+                        }
+                    }
+                    this.bitmap.UnlockBits(data);
+                   
+                }
+
+				this.Invalidate();
+			}
+		}
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().ToUpper() == Keys.Q.ToString())
+            {
+                Taskbar.Show();
+                Close();
+            }
+            base.OnKeyPress(e);
+        }
+		private readonly string SAMPLE_XML_FILE = @"SamplesConfig.xml";
+
+		private Context context;
+		private ScriptNode scriptNode;
+		private DepthGenerator depth;
+        private UserGenerator userGenerator;
+        private SkeletonCapability skeletonCapbility;
+        private PoseDetectionCapability poseDetectionCapability;
+        private string calibPose;
+		private Thread readerThread;
+		private bool shouldRun;
+		private Bitmap bitmap;
+		private int[] histogram;
+
+        private Dictionary<int, Dictionary<SkeletonJoint, SkeletonJointPosition>> joints;
+
+        private bool shouldDrawPixels = true;
+        private bool shouldDrawBackground = false;
+        private bool shouldPrintID = false;
+        private bool shouldPrintState = false;
+        private bool shouldDrawSkeleton = false;
+
+
+
+	}
+
+    
+}
---
+++ Utilities/Effect.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Reflection;
+using System.Drawing.Imaging;
+namespace GdipEffect
+{
+    // ***************** GDI+ Effect函數的示例代碼 *********************
+    // 作者     ： laviewpbt 
+    // 作者簡介 ： 對影像處理（非識別）有著較深程度的理解
+    // 使用語言 ： VB6.0/C#/VB.NET
+    // 聯繫方式 ： QQ-33184777  E-Mail:laviewpbt@sina.com
+    // 開發時間 ： 2012.12.10-2012.12.12
+    // 致謝     ： Aaron Lee Murgatroyd
+    // 版權聲明 ： 複製或轉載請保留以上個人資訊
+    // *****************************************************************
+
+    public static class Effect
+    {
+        private static Guid BlurEffectGuid = new Guid("{633C80A4-1843-482B-9EF2-BE2834C5FDD4}");
+        private static Guid UsmSharpenEffectGuid = new Guid("{63CBF3EE-C526-402C-8F71-62C540BF5142}");
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct BlurParameters
+        {
+            internal float Radius;
+            internal bool ExpandEdges;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SharpenParams
+        {
+            internal float Radius;
+            internal float Amount;
+        }
+
+        internal enum PaletteType               // GDI+1.1還可以針對一副圖像獲取某種特殊的調色
+        {
+            PaletteTypeCustom = 0,
+            PaletteTypeOptimal = 1,
+            PaletteTypeFixedBW = 2,
+            PaletteTypeFixedHalftone8 = 3,
+            PaletteTypeFixedHalftone27 = 4,
+            PaletteTypeFixedHalftone64 = 5,
+            PaletteTypeFixedHalftone125 = 6,
+            PaletteTypeFixedHalftone216 = 7,
+            PaletteTypeFixedHalftone252 = 8,
+            PaletteTypeFixedHalftone256 = 9
+        };
+
+        internal enum DitherType                    // 這個主要用於將真彩色圖像轉換為索引圖像，並儘量減低顏色損失
+        {
+            DitherTypeNone = 0,
+            DitherTypeSolid = 1,
+            DitherTypeOrdered4x4 = 2,
+            DitherTypeOrdered8x8 = 3,
+            DitherTypeOrdered16x16 = 4,
+            DitherTypeOrdered91x91 = 5,
+            DitherTypeSpiral4x4 = 6,
+            DitherTypeSpiral8x8 = 7,
+            DitherTypeDualSpiral4x4 = 8,
+            DitherTypeDualSpiral8x8 = 9,
+            DitherTypeErrorDiffusion = 10
+        }
+
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipCreateEffect(Guid guid, out IntPtr effect);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipDeleteEffect(IntPtr effect);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipGetEffectParameterSize(IntPtr effect, out uint size);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipSetEffectParameters(IntPtr effect, IntPtr parameters, uint size);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipGetEffectParameters(IntPtr effect, ref uint size, IntPtr parameters);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipBitmapApplyEffect(IntPtr bitmap, IntPtr effect, ref Rectangle rectOfInterest, bool useAuxData, IntPtr auxData, int auxDataSize);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipBitmapCreateApplyEffect(ref IntPtr SrcBitmap, int numInputs, IntPtr effect, ref Rectangle rectOfInterest, ref Rectangle outputRect, out IntPtr outputBitmap, bool useAuxData, IntPtr auxData, int auxDataSize);
+
+
+        // 這個函數我在C#下已經調用成功
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipInitializePalette(IntPtr palette, int palettetype, int optimalColors, int useTransparentColor, int bitmap);
+
+        // 該函數一致不成功，不過我在VB6下調用很簡單，也很成功，主要是結構體的問題。
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipBitmapConvertFormat(IntPtr bitmap, int pixelFormat, int dithertype, int palettetype, IntPtr palette, float alphaThresholdPercent);
+
+        /// <summary>
+        /// 獲取對象的私有欄位的值，感謝Aaron Lee Murgatroyd
+        /// </summary>
+        /// <typeparam name="TResult">欄位的類型</typeparam>
+        /// <param name="obj">要從其中獲取欄位值的對象</param>
+        /// <param name="fieldName">欄位的名稱.</param>
+        /// <returns>欄位的值</returns>
+        /// <exception cref="System.InvalidOperationException">無法找到該欄位.</exception>
+        /// 
+        internal static TResult GetPrivateField<TResult>(this object obj, string fieldName)
+        {
+            if (obj == null) return default(TResult);
+            Type ltType = obj.GetType();
+            FieldInfo lfiFieldInfo = ltType.GetField(fieldName, System.Reflection.BindingFlags.GetField | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            if (lfiFieldInfo != null)
+                return (TResult)lfiFieldInfo.GetValue(obj);
+            else
+                throw new InvalidOperationException(string.Format("Instance field '{0}' could not be located in object of type '{1}'.", fieldName, obj.GetType().FullName));
+        }
+
+        public static IntPtr NativeHandle(this Bitmap Bmp)
+        {
+            return Bmp.GetPrivateField<IntPtr>("nativeImage");
+            /*  用Reflector反編譯System.Drawing.Dll可以看到Image類有如下的私有欄位
+                internal IntPtr nativeImage;
+                private byte[] rawData;
+                private object userData;
+                然後還有一個 SetNativeImage函數
+                internal void SetNativeImage(IntPtr handle)
+                {
+                    if (handle == IntPtr.Zero)
+                    {
+                        throw new ArgumentException(SR.GetString("NativeHandle0"), "handle");
+                    }
+                    this.nativeImage = handle;
+                }
+                這裡在看看FromFile等等函數，其實也就是調用一些例如GdipLoadImageFromFile之類的GDIP函數，並把返回的GDIP圖像控制碼
+                通過調用SetNativeImage賦值給變數nativeImage，因此如果我們能獲得該值，就可以調用VS2010暫時還沒有封裝的GDIP函數
+                進行相關處理了，並且由於.NET肯定已經初始化過了GDI+，我們也就無需在調用GdipStartup初始化他了。
+             */
+        }
+
+        public static void Invert(this Bitmap bimage)
+        {
+            // Step 1: 先鎖住存放圖片的記憶體
+            BitmapData bmData = bimage.LockBits(new Rectangle(0, 0, bimage.Width, bimage.Height),
+            ImageLockMode.ReadWrite,
+           PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;
+            // Step 2: 取得像點資料的起始位址 
+            System.IntPtr Scan0 = bmData.Scan0;
+            // 計算每行的像點所佔據的byte 總數
+            int ByteNumber_Width = bimage.Width * 3;
+            // 計算每一行後面幾個 Padding bytes
+            int ByteOfSkip = stride - ByteNumber_Width;
+            // Step 3: 直接利用指標, 更改圖檔的內容
+            int Height = bimage.Height;
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < ByteNumber_Width; x++)
+                    {
+                        p[0] = (byte)(255 - p[0]); // 彩色資料反轉
+                        ++p;
+                    }
+                    p += ByteOfSkip; // 跳過剩下的 Padding bytes
+                }
+            }
+            bimage.UnlockBits(bmData);
+        }
+
+        public static void Lighter(this Bitmap bimage, int Times)
+        {
+            // Step 1: 先鎖住存放圖片的記憶體
+            BitmapData bmData = bimage.LockBits(new Rectangle(0, 0, bimage.Width, bimage.Height),
+            ImageLockMode.ReadWrite,
+           PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;
+            // Step 2: 取得像點資料的起始位址 
+            System.IntPtr Scan0 = bmData.Scan0;
+            // 計算每行的像點所佔據的byte 總數
+            int ByteNumber_Width = bimage.Width * 3;
+            // 計算每一行後面幾個 Padding bytes
+            int ByteOfSkip = stride - ByteNumber_Width;
+            // Step 3: 直接利用指標, 更改圖檔的內容
+            int Height = bimage.Height;
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < ByteNumber_Width; x++)
+                    {
+                        if (p[0] == 0)
+                        {
+                            p[0] = (byte)(p[0] + 25 * Times);
+                        }
+
+                        ++p;
+                    }
+                    p += ByteOfSkip; // 跳過剩下的 Padding bytes
+                }
+            }
+            bimage.UnlockBits(bmData);
+        }
+
+        /// <summary>
+        /// 對圖像進行高斯模糊,參考：http://msdn.microsoft.com/en-us/library/ms534057(v=vs.85).aspx
+        /// </summary>
+        /// <param name="Rect">需要模糊的區域，會對該值進行邊界的修正並返回.</param>
+        /// <param name="Radius">指定高斯卷積核的半徑，有效範圍[0，255],半徑越大，圖像變得越模糊.</param>
+        /// <param name="ExpandEdge">指定是否對邊界進行擴展，設置為True，在邊緣處可獲得較為柔和的效果. </param>
+
+        public static void GaussianBlur(this Bitmap Bmp, ref Rectangle Rect, float Radius = 10, bool ExpandEdge = false)
+        {
+            int Result;
+            IntPtr BlurEffect;
+            BlurParameters BlurPara;
+            if ((Radius < 0) || (Radius > 255))
+            {
+                throw new ArgumentOutOfRangeException("半徑必須在[0,255]範圍內");
+            }
+            BlurPara.Radius = Radius;
+            BlurPara.ExpandEdges = ExpandEdge;
+            Result = GdipCreateEffect(BlurEffectGuid, out BlurEffect);
+            if (Result == 0)
+            {
+                IntPtr Handle = Marshal.AllocHGlobal(Marshal.SizeOf(BlurPara));
+                Marshal.StructureToPtr(BlurPara, Handle, true);
+                GdipSetEffectParameters(BlurEffect, Handle, (uint)Marshal.SizeOf(BlurPara));
+                GdipBitmapApplyEffect(Bmp.NativeHandle(), BlurEffect, ref Rect, false, IntPtr.Zero, 0);
+                // 使用GdipBitmapCreateApplyEffect函數可以不改變原始的圖像，而把模糊的結果寫入到一個新的圖像中
+                GdipDeleteEffect(BlurEffect);
+                Marshal.FreeHGlobal(Handle);
+            }
+            else
+            {
+                throw new ExternalException("不支持的GDI+版本，必須為GDI+1.1及以上版本，且作業系統要求為Win Vista及之後版本.");
+            }
+        }
+
+
+        /// <summary>
+        /// 對圖像進行銳化,參考：http://msdn.microsoft.com/en-us/library/ms534073(v=vs.85).aspx
+        /// </summary>
+        /// <param name="Rect">需要銳化的區域，會對該值進行邊界的修正並返回.</param>
+        /// <param name="Radius">指定高斯卷積核的半徑，有效範圍[0，255],因為這個銳化演算法是以高斯模糊為基礎的，所以他的速度肯定比高斯模糊媽媽</param>
+        /// <param name="ExpandEdge">指定銳化的程度，0表示不銳化。有效範圍[0,255]. </param>
+        /// 
+        public static void UsmSharpen(this Bitmap Bmp, ref Rectangle Rect, float Radius = 10, float Amount = 50f)
+        {
+            int Result;
+            IntPtr UnSharpMaskEffect;
+            SharpenParams sharpenParams;
+            if ((Radius < 0) || (Radius > 255))
+            {
+                throw new ArgumentOutOfRangeException("參數Radius必須在[0,255]範圍內");
+            }
+            if ((Amount < 0) || (Amount > 100))
+            {
+                throw new ArgumentOutOfRangeException("參數Amount必須在[0,255]範圍內");
+            }
+            sharpenParams.Radius = Radius;
+            sharpenParams.Amount = Amount;
+            Result = GdipCreateEffect(UsmSharpenEffectGuid, out UnSharpMaskEffect);
+            if (Result == 0)
+            {
+                IntPtr Handle = Marshal.AllocHGlobal(Marshal.SizeOf(sharpenParams));
+                Marshal.StructureToPtr(sharpenParams, Handle, true);
+                GdipSetEffectParameters(UnSharpMaskEffect, Handle, (uint)Marshal.SizeOf(sharpenParams));
+                GdipBitmapApplyEffect(Bmp.NativeHandle(), UnSharpMaskEffect, ref Rect, false, IntPtr.Zero, 0);
+                GdipDeleteEffect(UnSharpMaskEffect);
+                Marshal.FreeHGlobal(Handle);
+            }
+            else
+            {
+                throw new ExternalException("不支持的GDI+版本，必須為GDI+1.1及以上版本，且作業系統要求為Win Vista及之後版本.");
+            }
+        }
+    }
+}
+
